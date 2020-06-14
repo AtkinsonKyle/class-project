b9b5f445e419a637e6344d3a55b87296ffbd1f8e
Kyle <kyleatkinson5@gmail.com>
2020-06-13 20:32:03 -0500

    added loop

--- function.js
+++ function.js
@@ -1,59 +1,58 @@
 function askName() {
     var username = prompt('What is your name?');
-    return document.write('<h3>Hello ' + username + '</h3>');
+    return document.write('<h3>Welcome' + username + '</h3>');
 }
-function addGreeting() {
+
+function greeting() {
     var today = new Date();
     var hourNow = today.getHours();
     var greeting;
     if (hourNow > 18) {
         greeting = 'Good Evening!';
-    }
-    else if (hourNow > 12) {
+    } else if (hourNow > 12) {
         greeting = 'Good Afternoon!';
-    }
-    else if (hourNow > 0) {
+    } else if (hourNow > 0) {
         greeting = 'Good Morning!';
-    }
-    else {
+    } else {
         greeting = 'Welcome!';
     }
-    return document.write('<h3>' + greeting + '</h3>');
+    return document.write('<h3> ' + greeting + '</h3>');
 }
 
-// get product (hotel or house)
-function getProduct() {
-    var response = prompt('great how many you want?');
-    var item;
-
-    while (response !== 'house' && response !== 'hotel') {
-        response = prompt(' please enter exactly! would you like a house or a hotel?');
-    } 
-    if(response === 'house'){
-        item = '<img src="circuit-board.jpg">';
-            } else if (response === 'hotel'){
-                item = '<img src="controller.jpg">';
-            }
-    return item;
-}
-// get count (how many they want)
 function getCount() {
-    var count = prompt('Great how many do you want?');
+    var count = prompt('How many would you like?');
 
     while (isNaN(count) || count === '') {
-        count = prompt('please enter a number, how many do you want?');
+        count = prompt('Please enter the exact number you would like.');
     }
     return count;
+
 }
-// show order - render to page
-function showOrder(){
+
+function getProduct() {
+    var response = prompt('Would you like to buy a meme or game?')
+    var item;
+    while (response !== 'meme' && response !== 'game') {
+
+    }
+    if (response === 'game') {
+        item = '<img src="controller.jpg">';
+    }
+    if (response === 'meme') {
+        item = '<img src="code-meme.jpg">';
+    }
+    return item;
+}
+
+function showOrder() {
     var result = ''
     var itemType = getProduct();
     var total = getCount();
 
-    for (var i = 0; i < total; i ++){
+    for (var i = 0; i < total; i++) {
         result = result + '<p>' + itemType + '</p>';
     }
+
     return document.write(result);
 }
 
@@ -72,34 +71,3 @@ function showOrder(){
 
 //    return document.write('<h3>' + message + '</h3>');
 //}
-
-function logger() {
-    var message = 'Hello ';
-    return message;
-}
-
-logger();
-
-function logUsername(username) {
-    var message = 'Hello ' + username + '!';
-    return message;
-}
-
-logUsername('Kyle');
-
-var loggerOne = function () {
-    var message = 'Hello ';
-    return message;
-}
-
-var logUsernameTwo = function (username) {
-    var message = 'Hello ' + username + '!';
-    return message;
-}
-
-var buildCake = function (cakeType, frosting, filling) {
-    var message = ('I need a ' + cakeType + ' cake frosted with ' + frosting + ' frosting and filled with ' + filling + '!');
-    return message;
-}
-
-buildCake('red velvet', 'cream cheese', 'vanilla ice cream');
